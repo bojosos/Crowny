36cc5df044b29809d59936a8f1e929f4f6492ccb
bojosos <ivanov6@abv.bg>
2022-04-19 11:59:29 +0300

    Memory tracking

--- Crowny/Source/Crowny/Memory/Allocator.cpp
+++ Crowny/Source/Crowny/Memory/Allocator.cpp
@@ -6,13 +6,114 @@
 namespace Crowny
 {
 
-    void* Allocator::Allocate(size_t size) noexcept
+	static AllocationStats s_GlobalStats;
+    static bool s_InInit = false;
+
+    void Allocator::Init()
     {
-        MemoryManager::Get().m_MemoryStats.TotalAllocated += size;
-        MemoryManager::Get().m_MemoryStats.CurrentAllcoated += size;
-        MemoryManager::Get().m_MemoryStats.Allocations++;
+        if (s_Data != nullptr)
+            return;
+        s_InInit = true;
+		AllocatorData* data = (AllocatorData*)Allocator::AllocateRaw(sizeof(AllocatorData));
+		new(data) AllocatorData();
+        s_Data = data;
+		s_InInit = false;
+    }
+
+    void* Allocator::AllocateRaw(size_t size)
+    {
         return malloc(size);
     }
 
-    void Allocator::Free(void* block) noexcept { free(block); }
+    void* Allocator::Allocate(size_t size)
+    {
+        if (s_InInit)
+			return AllocateRaw(size);
+		if (!s_Data)
+			Init();
+		void* memory = malloc(size);
+        {
+			Lock lock(s_Data->m_Mutex);
+            Allocation& alloc = s_Data->m_AllocationMap[memory];
+            alloc.Memory = memory;
+			alloc.Size = size;
+            s_GlobalStats.TotalAllocated += size;
+        }
+        return memory;
+    }
+
+	void* Allocator::Allocate(size_t size, const char* desc)
+	{
+		if (!s_Data)
+			Init();
+
+		void* memory = malloc(size);
+
+		{
+			std::scoped_lock<std::mutex> lock(s_Data->m_Mutex);
+			Allocation& alloc = s_Data->m_AllocationMap[memory];
+			alloc.Memory = memory;
+			alloc.Size = size;
+			alloc.Category = desc;
+
+			s_GlobalStats.TotalAllocated += size;
+			if (desc)
+				s_Data->m_AllocationStatsMap[desc].TotalAllocated += size;
+		}
+
+		return memory;
+	}
+
+	void* Allocator::Allocate(size_t size, const char* file, int line)
+	{
+		if (!s_Data)
+			Init();
+
+		void* memory = malloc(size);
+
+		{
+			std::scoped_lock<std::mutex> lock(s_Data->m_Mutex);
+			Allocation& alloc = s_Data->m_AllocationMap[memory];
+			alloc.Memory = memory;
+			alloc.Size = size;
+			alloc.Category = file;
+
+			s_GlobalStats.TotalAllocated += size;
+			s_Data->m_AllocationStatsMap[file].TotalAllocated += size;
+		}
+
+		return memory;
+	}
+
+	void Allocator::Free(void* memory)
+	{
+		if (memory == nullptr)
+			return;
+
+		if (s_Data->m_AllocationMap.find(memory) != s_Data->m_AllocationMap.end())
+		{
+			std::scoped_lock<std::mutex> lock(s_Data->m_Mutex);
+			const Allocation& alloc = s_Data->m_AllocationMap.at(memory);
+			s_GlobalStats.TotalFreed += alloc.Size;
+			if (alloc.Category)
+				s_Data->m_AllocationStatsMap[alloc.Category].TotalFreed += alloc.Size;
+
+			s_Data->m_AllocationMap.erase(memory);
+		}
+		else
+		{
+			CW_ENGINE_WARN("Memory: Memory block {0} not present in alloc map", memory);
+		}
+
+		free(memory);
+	}
+
+	namespace Memory
+	{
+		const Crowny::AllocationStats& GetAllocationStats()
+		{
+			return s_GlobalStats;
+		}
+	}
+
 } // namespace Crowny
--- Crowny/Source/Crowny/Memory/Allocator.h
+++ Crowny/Source/Crowny/Memory/Allocator.h
@@ -2,11 +2,75 @@
 
 namespace Crowny
 {
+	
+    struct AllocationStats
+    {
+		size_t TotalAllocated = 0;
+        size_t TotalFreed = 0;
+    };
+	
+    struct Allocation
+    {
+		void* Memory = 0;
+		size_t Size = 0;
+        const char* Category;
+    };
+
+	template <class T>
+	struct Mallocator
+	{
+		typedef T value_type;
+
+		Mallocator() = default;
+		template <class U> constexpr Mallocator(const Mallocator <U>&) noexcept {}
+
+		T* allocate(std::size_t n)
+		{
+#undef max
+			if (n > std::numeric_limits<std::size_t>::max() / sizeof(T))
+				throw std::bad_array_new_length();
+
+			if (auto p = static_cast<T*>(std::malloc(n * sizeof(T)))) {
+				return p;
+			}
+
+			throw std::bad_alloc();
+		}
+
+		void deallocate(T* p, std::size_t n) noexcept {
+			std::free(p);
+		}
+	};
+
+    struct AllocatorData
+    {
+		using MapAlloc = Mallocator<std::pair<const void* const, Allocation>>;
+		using StatsMapAlloc = Mallocator<std::pair<const char* const, AllocationStats>>;
+
+		using AllocationStatsMap = std::map<const char*, AllocationStats, std::less<const char*>, StatsMapAlloc>;
+		std::map<const void*, Allocation, std::less<const void*>, MapAlloc> m_AllocationMap;
+		AllocationStatsMap m_AllocationStatsMap;
+        Mutex m_Mutex, m_StatsMutex;
+    };
+
+	namespace Memory
+	{
+		const AllocationStats& GetAllocationStats();
+	}
+
     class Allocator
     {
     public:
-        static void* Allocate(size_t size) noexcept;
+        static void Init();
+        static void* AllocateRaw(size_t size);
+        static void* Allocate(size_t size);
+        static void* Allocate(size_t size, const char* desc);
+        static void* Allocate(size_t size, const char* file, int line);
 
-        static void Free(void* block) noexcept;
+        static void Free(void* block);
+		static const AllocatorData::AllocationStatsMap& GetAllocationStats() { return s_Data->m_AllocationStatsMap; }
+    private:
+		inline static AllocatorData* s_Data = nullptr;
     };
+
 } // namespace Crowny
